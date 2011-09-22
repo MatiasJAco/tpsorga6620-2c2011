343060cbd46094d9cdb9896ab973a1cf0de0c243
maldonado.celeste@gmail.com <maldonado.celeste@gmail.com@a299ed4a-ec92-d95f-397f-5777b1f096ef>
2011-09-22 16:16:27 +0000

    modificaciones en la introduccion del informe, y el resultado de las pruebas...
    tb subo el assembly nuevo generado.
    
    igual hay que revisar si esta andando bien.

--- trunk/ tpsorga6620-2c2011 --username MatiasJA@gmail.com/tp0/assembly/tp0.s
+++ trunk/ tpsorga6620-2c2011 --username MatiasJA@gmail.com/tp0/assembly/tp0.s
@@ -39,30 +39,36 @@ $LC3:
 	.ascii	"hVi:\000"
 	.align	2
 $LC4:
+	.ascii	"No se han especificado parametros, para ayuda ejecute el"
+	.ascii	" programa ./tp0 \n\000"
+	.align	2
+$LC5:
 	.ascii	"Version 1.00\n\000"
 	.text
 	.align	2
 	.globl	main
 	.ent	main
 main:
-	.frame	$fp,208,$ra		# vars= 160, regs= 3/0, args= 24, extra= 8
+	.frame	$fp,104,$ra		# vars= 56, regs= 3/0, args= 24, extra= 8
 	.mask	0xd0000000,-8
 	.fmask	0x00000000,0
 	.set	noreorder
 	.cpload	$t9
 	.set	reorder
-	subu	$sp,$sp,208
+	subu	$sp,$sp,104
 	.cprestore 24
-	sw	$ra,200($sp)
-	sw	$fp,196($sp)
-	sw	$gp,192($sp)
+	sw	$ra,96($sp)
+	sw	$fp,92($sp)
+	sw	$gp,88($sp)
 	move	$fp,$sp
-	sw	$a0,208($fp)
-	sw	$a1,212($fp)
+	sw	$a0,104($fp)
+	sw	$a1,108($fp)
 	sw	$zero,32($fp)
 	sw	$zero,36($fp)
 	li	$v0,1			# 0x1
 	sw	$v0,52($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,56($fp)
 	li	$v0,1			# 0x1
 	sw	$v0,48($fp)
 	li	$v0,1			# 0x1
@@ -72,36 +78,36 @@ main:
 $L18:
 	addu	$v0,$fp,36
 	sw	$v0,16($sp)
-	lw	$a0,208($fp)
-	lw	$a1,212($fp)
+	lw	$a0,104($fp)
+	lw	$a1,108($fp)
 	la	$a2,$LC3
 	la	$a3,long_options.0
 	la	$t9,getopt_long
 	jal	$ra,$t9
-	sw	$v0,56($fp)
-	lw	$v1,56($fp)
+	sw	$v0,60($fp)
+	lw	$v1,60($fp)
 	li	$v0,-1			# 0xffffffffffffffff
 	bne	$v1,$v0,$L20
 	b	$L19
 $L20:
-	lw	$v0,56($fp)
-	sw	$v0,188($fp)
+	lw	$v0,60($fp)
+	sw	$v0,84($fp)
 	li	$v0,104			# 0x68
-	lw	$v1,188($fp)
+	lw	$v1,84($fp)
 	beq	$v1,$v0,$L22
-	lw	$v1,188($fp)
+	lw	$v1,84($fp)
 	slt	$v0,$v1,105
 	beq	$v0,$zero,$L28
 	li	$v0,63			# 0x3f
-	lw	$v1,188($fp)
+	lw	$v1,84($fp)
 	beq	$v1,$v0,$L25
 	li	$v0,86			# 0x56
-	lw	$v1,188($fp)
+	lw	$v1,84($fp)
 	beq	$v1,$v0,$L23
 	b	$L18
 $L28:
 	li	$v0,105			# 0x69
-	lw	$v1,188($fp)
+	lw	$v1,84($fp)
 	beq	$v1,$v0,$L24
 	b	$L18
 $L22:
@@ -115,133 +121,260 @@ $L24:
 	b	$L18
 $L25:
 	li	$v0,1			# 0x1
-	sw	$v0,184($fp)
+	sw	$v0,80($fp)
 	b	$L17
 $L19:
-	lw	$v0,44($fp)
-	bne	$v0,$zero,$L29
+	lw	$v1,104($fp)
+	li	$v0,1			# 0x1
+	bne	$v1,$v0,$L29
 	la	$a0,$LC4
 	la	$t9,printf
 	jal	$ra,$t9
-	sw	$zero,184($fp)
+	sw	$zero,80($fp)
 	b	$L17
 $L29:
-	lw	$v0,40($fp)
+	lw	$v0,44($fp)
 	bne	$v0,$zero,$L30
-	la	$t9,print_help
+	la	$a0,$LC5
+	la	$t9,printf
 	jal	$ra,$t9
-	sw	$zero,184($fp)
+	sw	$zero,80($fp)
 	b	$L17
 $L30:
-	lw	$v0,48($fp)
+	lw	$v0,40($fp)
 	bne	$v0,$zero,$L31
+	la	$t9,print_help
+	jal	$ra,$t9
+	sw	$zero,80($fp)
+	b	$L17
+$L31:
+	lw	$v0,48($fp)
+	bne	$v0,$zero,$L32
 	sw	$zero,52($fp)
 	li	$v0,1			# 0x1
 	sw	$v0,32($fp)
-$L31:
+$L32:
 	lw	$v0,32($fp)
 	addu	$v0,$v0,1
-	sw	$v0,176($fp)
+	sw	$v0,72($fp)
 	lw	$v0,32($fp)
 	addu	$v0,$v0,2
-	sw	$v0,180($fp)
-	lw	$v1,208($fp)
-	lw	$v0,176($fp)
-	slt	$v0,$v0,$v1
-	beq	$v0,$zero,$L32
-	lw	$v0,176($fp)
-	sll	$v1,$v0,2
-	lw	$v0,212($fp)
-	addu	$v0,$v1,$v0
-	lw	$v0,0($v0)
-	sw	$v0,60($fp)
-	lw	$v1,208($fp)
-	lw	$v0,180($fp)
+	sw	$v0,76($fp)
+	lw	$v1,104($fp)
+	lw	$v0,72($fp)
 	slt	$v0,$v0,$v1
 	beq	$v0,$zero,$L33
-	lw	$v0,180($fp)
+	lw	$v0,72($fp)
 	sll	$v1,$v0,2
-	lw	$v0,212($fp)
+	lw	$v0,108($fp)
 	addu	$v0,$v1,$v0
 	lw	$v0,0($v0)
 	sw	$v0,64($fp)
-	b	$L34
-$L33:
-	addu	$v0,$fp,72
-	move	$a0,$v0
-	li	$a1,100			# 0x64
-	la	$a2,__sF
-	la	$t9,fgets
-	jal	$ra,$t9
-	addu	$v0,$fp,72
-	move	$a0,$v0
-	la	$t9,limpiar
-	jal	$ra,$t9
-	addu	$v0,$fp,72
-	sw	$v0,64($fp)
+	lw	$v1,104($fp)
+	lw	$v0,76($fp)
+	slt	$v0,$v0,$v1
+	beq	$v0,$zero,$L34
+	lw	$v0,76($fp)
+	sll	$v1,$v0,2
+	lw	$v0,108($fp)
+	addu	$v0,$v1,$v0
+	lw	$v0,0($v0)
+	sw	$v0,68($fp)
+	b	$L35
 $L34:
-	lw	$a0,60($fp)
-	lw	$a1,64($fp)
+	sw	$zero,56($fp)
+$L35:
+	lw	$a0,64($fp)
+	lw	$a1,68($fp)
 	lw	$a2,52($fp)
+	lw	$a3,56($fp)
 	la	$t9,funcionJoin
 	jal	$ra,$t9
-$L32:
-	sw	$zero,184($fp)
+$L33:
+	sw	$zero,80($fp)
 $L17:
-	lw	$v0,184($fp)
+	lw	$v0,80($fp)
 	move	$sp,$fp
-	lw	$ra,200($sp)
-	lw	$fp,196($sp)
-	addu	$sp,$sp,208
+	lw	$ra,96($sp)
+	lw	$fp,92($sp)
+	addu	$sp,$sp,104
 	j	$ra
 	.end	main
 	.size	main, .-main
 	.rdata
 	.align	2
-$LC5:
+$LC6:
+	.ascii	" \000"
+	.align	2
+$LC7:
+	.ascii	"\000\000"
+	.text
+	.align	2
+	.globl	checkRepetition
+	.ent	checkRepetition
+checkRepetition:
+	.frame	$fp,288,$ra		# vars= 248, regs= 3/0, args= 16, extra= 8
+	.mask	0xd0000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.cpload	$t9
+	.set	reorder
+	subu	$sp,$sp,288
+	.cprestore 16
+	sw	$ra,280($sp)
+	sw	$fp,276($sp)
+	sw	$gp,272($sp)
+	move	$fp,$sp
+	sw	$a0,288($fp)
+	sw	$a1,292($fp)
+	sw	$a2,296($fp)
+	sw	$a3,300($fp)
+	sw	$zero,24($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,28($fp)
+$L37:
+	addu	$v0,$fp,32
+	move	$a0,$v0
+	li	$a1,100			# 0x64
+	lw	$a2,288($fp)
+	la	$t9,fgets
+	jal	$ra,$t9
+	beq	$v0,$zero,$L38
+	lw	$v1,28($fp)
+	li	$v0,1			# 0x1
+	beq	$v1,$v0,$L39
+	b	$L38
+$L39:
+	addu	$v0,$fp,32
+	move	$a0,$v0
+	la	$a1,$LC6
+	la	$t9,strtok
+	jal	$ra,$t9
+	sw	$v0,264($fp)
+	addu	$v0,$fp,240
+	move	$a0,$v0
+	lw	$a1,264($fp)
+	la	$t9,strcpy
+	jal	$ra,$t9
+	move	$a0,$zero
+	la	$a1,$LC7
+	la	$t9,strtok
+	jal	$ra,$t9
+	sw	$v0,264($fp)
+	addu	$v0,$fp,136
+	move	$a0,$v0
+	lw	$a1,264($fp)
+	la	$t9,strcpy
+	jal	$ra,$t9
+	addu	$v0,$fp,136
+	move	$a0,$v0
+	la	$t9,limpiar
+	jal	$ra,$t9
+	lw	$v0,24($fp)
+	sll	$v1,$v0,2
+	lw	$v0,292($fp)
+	addu	$v0,$v1,$v0
+	addu	$v1,$fp,240
+	lw	$a0,0($v0)
+	move	$a1,$v1
+	la	$t9,strcpy
+	jal	$ra,$t9
+	lw	$v0,24($fp)
+	sll	$v1,$v0,2
+	lw	$v0,296($fp)
+	addu	$v0,$v1,$v0
+	addu	$v1,$fp,136
+	lw	$a0,0($v0)
+	move	$a1,$v1
+	la	$t9,strcpy
+	jal	$ra,$t9
+	lw	$v0,24($fp)
+	addu	$v0,$v0,1
+	sw	$v0,24($fp)
+	sw	$zero,268($fp)
+	sw	$zero,268($fp)
+$L41:
+	lw	$v0,24($fp)
+	addu	$v1,$v0,-1
+	lw	$v0,268($fp)
+	slt	$v0,$v0,$v1
+	bne	$v0,$zero,$L44
+	b	$L37
+$L44:
+	lw	$v0,268($fp)
+	sll	$v1,$v0,2
+	lw	$v0,292($fp)
+	addu	$v0,$v1,$v0
+	addu	$v1,$fp,240
+	lw	$a0,0($v0)
+	move	$a1,$v1
+	lw	$a2,300($fp)
+	la	$t9,comparaClaves
+	jal	$ra,$t9
+	sw	$v0,28($fp)
+	lw	$v0,28($fp)
+	bne	$v0,$zero,$L43
+	b	$L37
+$L43:
+	lw	$v0,268($fp)
+	addu	$v0,$v0,1
+	sw	$v0,268($fp)
+	b	$L41
+$L38:
+	lw	$v0,28($fp)
+	move	$sp,$fp
+	lw	$ra,280($sp)
+	lw	$fp,276($sp)
+	addu	$sp,$sp,288
+	j	$ra
+	.end	checkRepetition
+	.size	checkRepetition, .-checkRepetition
+	.rdata
+	.align	2
+$LC8:
 	.ascii	"\n"
 	.ascii	"Usage: join [OPTION]... FILE1 FILE2\n\000"
 	.align	2
-$LC6:
+$LC9:
 	.ascii	"For each pair of input lines with identical join fields,"
 	.ascii	" write a line to\n\000"
 	.align	2
-$LC7:
+$LC10:
 	.ascii	"standard output. The default join field is the first, de"
 	.ascii	"limited\n\000"
 	.align	2
-$LC8:
+$LC11:
 	.ascii	"by whitespace. When FILE1 or FILE2 (not both) is -, read"
 	.ascii	" standard input.\n\000"
 	.align	2
-$LC9:
+$LC12:
 	.ascii	"  -i, --ignore-case  ignore differences in case when com"
 	.ascii	"paring fields\n\000"
 	.align	2
-$LC10:
+$LC13:
 	.ascii	"  -h, --help         display this help and exit\n\000"
 	.align	2
-$LC11:
+$LC14:
 	.ascii	"  -v, --version      output version information and exit"
 	.ascii	"\n\000"
 	.align	2
-$LC12:
+$LC15:
 	.ascii	"Important: FILE1 and FILE2 must be sorted on the join fi"
 	.ascii	"elds.\n\000"
 	.align	2
-$LC13:
+$LC16:
 	.ascii	"E.g., use \342\200\230sort -k 1b,1\342\200\231 if \342\200"
 	.ascii	"\230join\342\200\231 has no options.\n\000"
 	.align	2
-$LC14:
+$LC17:
 	.ascii	"Note, comparisons honor the rules specified by \342\200\230"
 	.ascii	"LC_COLLATE\342\200\231.\n\000"
 	.align	2
-$LC15:
+$LC18:
 	.ascii	"If the input is not sorted and some lines cannot be join"
 	.ascii	"ed, a\n\000"
 	.align	2
-$LC16:
+$LC19:
 	.ascii	"warning message will be given.\n\n\000"
 	.text
 	.align	2
@@ -260,15 +393,6 @@ print_help:
 	sw	$fp,28($sp)
 	sw	$gp,24($sp)
 	move	$fp,$sp
-	la	$a0,$LC5
-	la	$t9,printf
-	jal	$ra,$t9
-	la	$a0,$LC6
-	la	$t9,printf
-	jal	$ra,$t9
-	la	$a0,$LC7
-	la	$t9,printf
-	jal	$ra,$t9
 	la	$a0,$LC8
 	la	$t9,printf
 	jal	$ra,$t9
@@ -294,6 +418,15 @@ print_help:
 	la	$t9,printf
 	jal	$ra,$t9
 	la	$a0,$LC16
+	la	$t9,printf
+	jal	$ra,$t9
+	la	$a0,$LC17
+	la	$t9,printf
+	jal	$ra,$t9
+	la	$a0,$LC18
+	la	$t9,printf
+	jal	$ra,$t9
+	la	$a0,$LC19
 	la	$t9,printf
 	jal	$ra,$t9
 	move	$sp,$fp
@@ -326,10 +459,10 @@ limpiar:
 	jal	$ra,$t9
 	sw	$v0,24($fp)
 	lw	$v0,24($fp)
-	beq	$v0,$zero,$L36
+	beq	$v0,$zero,$L47
 	lw	$v0,24($fp)
 	sb	$zero,0($v0)
-$L36:
+$L47:
 	move	$sp,$fp
 	lw	$ra,40($sp)
 	lw	$fp,36($sp)
@@ -337,11 +470,6 @@ $L36:
 	j	$ra
 	.end	limpiar
 	.size	limpiar, .-limpiar
-	.rdata
-	.align	2
-$LC17:
-	.ascii	" \000"
-	.text
 	.align	2
 	.globl	buscarRepetida
 	.ent	buscarRepetida
@@ -370,21 +498,21 @@ buscarRepetida:
 	lw	$a2,188($fp)
 	la	$t9,fgets
 	jal	$ra,$t9
-$L39:
+$L50:
 	lw	$v0,164($fp)
 	slt	$v0,$v0,2
-	beq	$v0,$zero,$L40
+	beq	$v0,$zero,$L51
 	lw	$v0,188($fp)
 	lhu	$v0,12($v0)
 	srl	$v0,$v0,5
 	xori	$v0,$v0,0x1
 	andi	$v0,$v0,0x1
-	bne	$v0,$zero,$L41
-	b	$L40
-$L41:
+	bne	$v0,$zero,$L52
+	b	$L51
+$L52:
 	addu	$v0,$fp,32
 	move	$a0,$v0
-	la	$a1,$LC17
+	la	$a1,$LC6
 	la	$t9,strtok
 	jal	$ra,$t9
 	sw	$v0,160($fp)
@@ -399,24 +527,24 @@ $L41:
 	lw	$a2,192($fp)
 	la	$t9,comparaClaves
 	jal	$ra,$t9
-	bne	$v0,$zero,$L43
+	bne	$v0,$zero,$L54
 	lw	$v0,164($fp)
 	addu	$v0,$v0,1
 	sw	$v0,164($fp)
-$L43:
+$L54:
 	addu	$v0,$fp,32
 	move	$a0,$v0
 	li	$a1,100			# 0x64
 	lw	$a2,188($fp)
 	la	$t9,fgets
 	jal	$ra,$t9
-	b	$L39
-$L40:
+	b	$L50
+$L51:
 	lw	$v0,164($fp)
 	slt	$v0,$v0,2
-	bne	$v0,$zero,$L44
+	bne	$v0,$zero,$L55
 	sw	$zero,24($fp)
-$L44:
+$L55:
 	lw	$v0,24($fp)
 	move	$sp,$fp
 	lw	$ra,176($sp)
@@ -427,270 +555,365 @@ $L44:
 	.size	buscarRepetida, .-buscarRepetida
 	.rdata
 	.align	2
-$LC18:
+$LC20:
 	.ascii	"r\000"
 	.align	2
-$LC19:
-	.ascii	"Error al abrir %s \n\000"
-	.align	2
-$LC20:
-	.ascii	"\000\000"
-	.align	2
 $LC21:
-	.ascii	"%s %s %s \n\000"
+	.ascii	"\000"
+	.space	19
 	.align	2
 $LC22:
-	.ascii	"Clave repetida en el archivo2. \n\000"
+	.ascii	"Error al abrir %s \n\000"
 	.align	2
 $LC23:
-	.ascii	"El archivo 2 est\303\241 desordenado. \n\000"
+	.ascii	"%s %s %s \n\000"
 	.align	2
 $LC24:
+	.ascii	"Clave repetida en el archivo2. \n\000"
+	.align	2
+$LC25:
+	.ascii	"El archivo 1 est\303\241 desordenado. \n\000"
+	.align	2
+$LC26:
+	.ascii	"El archivo 2 est\303\241 desordenado. \n\000"
+	.align	2
+$LC27:
 	.ascii	"No se encontro la clave en el segundo archivo \n\000"
 	.text
 	.align	2
 	.globl	funcionJoin
 	.ent	funcionJoin
 funcionJoin:
-	.frame	$fp,672,$ra		# vars= 632, regs= 3/0, args= 16, extra= 8
-	.mask	0xd0000000,-8
+	.frame	$fp,1144,$ra		# vars= 1104, regs= 4/0, args= 16, extra= 8
+	.mask	0xd0010000,-4
 	.fmask	0x00000000,0
 	.set	noreorder
 	.cpload	$t9
 	.set	reorder
-	subu	$sp,$sp,672
+	subu	$sp,$sp,1144
 	.cprestore 16
-	sw	$ra,664($sp)
-	sw	$fp,660($sp)
-	sw	$gp,656($sp)
+	sw	$ra,1140($sp)
+	sw	$fp,1136($sp)
+	sw	$gp,1132($sp)
+	sw	$s0,1128($sp)
 	move	$fp,$sp
-	sw	$a0,672($fp)
-	sw	$a1,676($fp)
-	sw	$a2,680($fp)
-	lw	$a0,672($fp)
-	la	$a1,$LC18
+	sw	$a0,1144($fp)
+	sw	$a1,1148($fp)
+	sw	$a2,1152($fp)
+	sw	$a3,1156($fp)
+	lw	$a0,1144($fp)
+	la	$a1,$LC20
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,24($fp)
-	lw	$a0,676($fp)
-	la	$a1,$LC18
+	lw	$v0,1156($fp)
+	bne	$v0,$zero,$L57
+	la	$v0,__sF
+	sw	$v0,28($fp)
+	b	$L58
+$L57:
+	lw	$a0,1148($fp)
+	la	$a1,$LC20
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,28($fp)
-	lw	$a0,676($fp)
-	la	$a1,$LC18
-	la	$t9,fopen
+$L58:
+	sw	$zero,432($fp)
+	sw	$zero,432($fp)
+$L59:
+	lw	$v0,432($fp)
+	slt	$v0,$v0,100
+	bne	$v0,$zero,$L62
+	b	$L60
+$L62:
+	lw	$v0,432($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$s0,$v0,8
+	li	$a0,21			# 0x15
+	la	$t9,malloc
 	jal	$ra,$t9
-	sw	$v0,32($fp)
+	sw	$v0,0($s0)
+	lw	$v0,432($fp)
+	addu	$v0,$v0,1
+	sw	$v0,432($fp)
+	b	$L59
+$L60:
+	sw	$zero,432($fp)
+$L63:
+	lw	$v0,432($fp)
+	slt	$v0,$v0,100
+	bne	$v0,$zero,$L66
+	b	$L64
+$L66:
+	lw	$v0,432($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$s0,$v0,416
+	li	$a0,101			# 0x65
+	la	$t9,malloc
+	jal	$ra,$t9
+	sw	$v0,0($s0)
+	lw	$v0,432($fp)
+	addu	$v0,$v0,1
+	sw	$v0,432($fp)
+	b	$L63
+$L64:
 	li	$v0,1			# 0x1
-	sw	$v0,632($fp)
+	sw	$v0,1072($fp)
 	li	$v0,1			# 0x1
-	sw	$v0,636($fp)
+	sw	$v0,1076($fp)
 	li	$v0,1			# 0x1
-	sw	$v0,640($fp)
-	sw	$zero,644($fp)
+	sw	$v0,1080($fp)
+	sw	$zero,1084($fp)
 	li	$v0,1			# 0x1
-	sw	$v0,648($fp)
+	sw	$v0,1088($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,1092($fp)
+	lbu	$v0,$LC21
+	sb	$v0,1096($fp)
+	addu	$v0,$fp,1097
+	li	$v1,19			# 0x13
+	move	$a0,$v0
+	move	$a1,$zero
+	move	$a2,$v1
+	la	$t9,memset
+	jal	$ra,$t9
 	lw	$v0,24($fp)
-	bne	$v0,$zero,$L46
+	bne	$v0,$zero,$L67
 	la	$a0,__sF+176
-	la	$a1,$LC19
-	lw	$a2,672($fp)
+	la	$a1,$LC22
+	lw	$a2,1144($fp)
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,1			# 0x1
-	sw	$v0,644($fp)
-$L46:
+	sw	$v0,1084($fp)
+$L67:
 	lw	$v0,28($fp)
-	bne	$v0,$zero,$L47
+	bne	$v0,$zero,$L68
 	la	$a0,__sF+176
-	la	$a1,$LC19
-	lw	$a2,676($fp)
+	la	$a1,$LC22
+	lw	$a2,1148($fp)
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,1			# 0x1
-	sw	$v0,644($fp)
-$L47:
-	lw	$v0,644($fp)
-	bne	$v0,$zero,$L45
-	addu	$v0,$fp,320
+	sw	$v0,1084($fp)
+$L68:
+	sw	$zero,1124($fp)
+	lw	$v0,1084($fp)
+	bne	$v0,$zero,$L56
+	addu	$v0,$fp,32
+	addu	$v1,$fp,440
+	lw	$a0,28($fp)
+	move	$a1,$v0
+	move	$a2,$v1
+	lw	$a3,1152($fp)
+	la	$t9,checkRepetition
+	jal	$ra,$t9
+	sw	$v0,1076($fp)
+	lw	$v0,1076($fp)
+	bne	$v0,$zero,$L70
+	sw	$zero,1080($fp)
+$L70:
+	addu	$v0,$fp,968
 	move	$a0,$v0
 	li	$a1,100			# 0x64
 	lw	$a2,24($fp)
 	la	$t9,fgets
 	jal	$ra,$t9
-	addu	$v0,$fp,320
+	addu	$v0,$fp,968
 	move	$a0,$v0
-	la	$a1,$LC17
+	la	$a1,$LC6
 	la	$t9,strtok
 	jal	$ra,$t9
-	sw	$v0,652($fp)
-	addu	$v0,$fp,40
+	sw	$v0,1120($fp)
+	addu	$v0,$fp,840
 	move	$a0,$v0
-	lw	$a1,652($fp)
+	lw	$a1,1120($fp)
 	la	$t9,strcpy
 	jal	$ra,$t9
-$L49:
+$L71:
 	lw	$v0,24($fp)
 	lhu	$v0,12($v0)
 	srl	$v0,$v0,5
 	xori	$v0,$v0,0x1
 	andi	$v0,$v0,0x1
-	beq	$v0,$zero,$L50
-	lw	$v1,640($fp)
+	beq	$v0,$zero,$L72
+	lw	$v1,1080($fp)
 	li	$v0,1			# 0x1
-	beq	$v1,$v0,$L51
-	b	$L50
-$L51:
+	beq	$v1,$v0,$L73
+	b	$L72
+$L73:
 	move	$a0,$zero
-	la	$a1,$LC20
+	la	$a1,$LC7
 	la	$t9,strtok
 	jal	$ra,$t9
-	sw	$v0,652($fp)
-	addu	$v0,$fp,112
+	sw	$v0,1120($fp)
+	addu	$v0,$fp,864
 	move	$a0,$v0
-	lw	$a1,652($fp)
+	lw	$a1,1120($fp)
 	la	$t9,strcpy
 	jal	$ra,$t9
-	addu	$v0,$fp,112
+	addu	$v0,$fp,864
 	move	$a0,$v0
 	la	$t9,limpiar
 	jal	$ra,$t9
-	addu	$v0,$fp,424
-	move	$a0,$v0
-	li	$a1,100			# 0x64
-	lw	$a2,28($fp)
-	la	$t9,fgets
-	jal	$ra,$t9
-	addu	$v0,$fp,424
-	move	$a0,$v0
-	la	$a1,$LC17
-	la	$t9,strtok
-	jal	$ra,$t9
-	sw	$v0,652($fp)
-	addu	$v0,$fp,88
-	move	$a0,$v0
-	lw	$a1,652($fp)
-	la	$t9,strcpy
-	jal	$ra,$t9
-	addu	$v0,$fp,40
-	addu	$v1,$fp,88
-	move	$a0,$v0
-	move	$a1,$v1
-	lw	$a2,680($fp)
+	addu	$a0,$fp,840
+	lw	$v0,1124($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$v0,$v0,8
+	lw	$a1,0($v0)
+	lw	$a2,1152($fp)
 	la	$t9,comparaClaves
 	jal	$ra,$t9
-	bne	$v0,$zero,$L53
-	sw	$zero,632($fp)
-	addu	$v0,$fp,64
+	bne	$v0,$zero,$L75
+	sw	$zero,1072($fp)
+	b	$L76
+$L75:
+	sw	$zero,1088($fp)
+$L76:
+	addu	$v0,$fp,840
+	addu	$v1,$fp,1096
 	move	$a0,$v0
-	lw	$a1,652($fp)
-	la	$t9,strcpy
+	move	$a1,$v1
+	lw	$a2,1152($fp)
+	la	$t9,comparaClavesConsec
 	jal	$ra,$t9
-	move	$a0,$zero
-	la	$a1,$LC20
-	la	$t9,strtok
-	jal	$ra,$t9
-	sw	$v0,652($fp)
-	addu	$v0,$fp,216
-	move	$a0,$v0
-	lw	$a1,652($fp)
-	la	$t9,strcpy
-	jal	$ra,$t9
-	addu	$v0,$fp,216
-	move	$a0,$v0
-	la	$t9,limpiar
-	jal	$ra,$t9
-	lw	$v0,632($fp)
-	bne	$v0,$zero,$L55
-	addu	$v0,$fp,88
-	move	$a0,$v0
-	lw	$a1,32($fp)
-	lw	$a2,680($fp)
-	la	$t9,buscarRepetida
-	jal	$ra,$t9
-	sw	$v0,636($fp)
-	b	$L55
-$L53:
-	sw	$zero,648($fp)
-$L55:
-	lw	$v0,632($fp)
-	bne	$v0,$zero,$L56
-	lw	$v1,636($fp)
+	move	$v1,$v0
 	li	$v0,1			# 0x1
-	bne	$v1,$v0,$L56
-	addu	$v0,$fp,40
-	addu	$v1,$fp,112
-	addu	$a3,$fp,216
-	la	$a0,$LC21
-	move	$a1,$v0
-	move	$a2,$v1
+	bne	$v1,$v0,$L77
+	sw	$zero,1092($fp)
+$L77:
+	addu	$v0,$fp,1096
+	addu	$v1,$fp,840
+	move	$a0,$v0
+	move	$a1,$v1
+	la	$t9,strcpy
+	jal	$ra,$t9
+	lw	$v0,1072($fp)
+	bne	$v0,$zero,$L78
+	lw	$v1,1076($fp)
+	li	$v0,1			# 0x1
+	bne	$v1,$v0,$L78
+	lw	$v1,1092($fp)
+	li	$v0,1			# 0x1
+	bne	$v1,$v0,$L78
+	addu	$a1,$fp,840
+	addu	$a2,$fp,864
+	lw	$v0,1124($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$v0,$v0,416
+	la	$a0,$LC23
+	lw	$a3,0($v0)
 	la	$t9,printf
 	jal	$ra,$t9
 	li	$v0,1			# 0x1
-	sw	$v0,632($fp)
-	addu	$v0,$fp,320
+	sw	$v0,1072($fp)
+	lw	$v0,1124($fp)
+	addu	$v0,$v0,1
+	sw	$v0,1124($fp)
+	addu	$v0,$fp,968
 	move	$a0,$v0
 	li	$a1,100			# 0x64
 	lw	$a2,24($fp)
 	la	$t9,fgets
 	jal	$ra,$t9
-	addu	$v0,$fp,320
+	addu	$v0,$fp,968
 	move	$a0,$v0
-	la	$a1,$LC17
+	la	$a1,$LC6
 	la	$t9,strtok
 	jal	$ra,$t9
-	sw	$v0,652($fp)
-	addu	$v0,$fp,40
+	sw	$v0,1120($fp)
+	addu	$v0,$fp,840
 	move	$a0,$v0
-	lw	$a1,652($fp)
+	lw	$a1,1120($fp)
 	la	$t9,strcpy
 	jal	$ra,$t9
-	lw	$a0,32($fp)
-	la	$t9,rewind
-	jal	$ra,$t9
-	b	$L49
-$L56:
-	sw	$zero,640($fp)
-	b	$L49
-$L50:
-	lw	$v0,640($fp)
-	bne	$v0,$zero,$L58
-	lw	$v0,636($fp)
-	bne	$v0,$zero,$L59
-	la	$a0,__sF+176
-	la	$a1,$LC22
-	la	$t9,fprintf
-	jal	$ra,$t9
-	b	$L58
-$L59:
-	lw	$v0,648($fp)
-	bne	$v0,$zero,$L61
-	la	$a0,__sF+176
-	la	$a1,$LC23
-	la	$t9,fprintf
-	jal	$ra,$t9
-	b	$L58
-$L61:
+	b	$L71
+$L78:
+	sw	$zero,1080($fp)
+	b	$L71
+$L72:
+	lw	$v0,1080($fp)
+	bne	$v0,$zero,$L80
+	lw	$v0,1076($fp)
+	bne	$v0,$zero,$L81
 	la	$a0,__sF+176
 	la	$a1,$LC24
 	la	$t9,fprintf
 	jal	$ra,$t9
-$L58:
+	b	$L80
+$L81:
+	lw	$v0,1092($fp)
+	bne	$v0,$zero,$L83
+	la	$a0,__sF+176
+	la	$a1,$LC25
+	la	$t9,fprintf
+	jal	$ra,$t9
+	b	$L80
+$L83:
+	lw	$v0,1088($fp)
+	bne	$v0,$zero,$L85
+	la	$a0,__sF+176
+	la	$a1,$LC26
+	la	$t9,fprintf
+	jal	$ra,$t9
+	b	$L80
+$L85:
+	la	$a0,__sF+176
+	la	$a1,$LC27
+	la	$t9,fprintf
+	jal	$ra,$t9
+$L80:
 	lw	$a0,24($fp)
 	la	$t9,fclose
 	jal	$ra,$t9
+	lw	$v1,1156($fp)
+	li	$v0,1			# 0x1
+	bne	$v1,$v0,$L87
 	lw	$a0,28($fp)
 	la	$t9,fclose
 	jal	$ra,$t9
-	lw	$a0,32($fp)
-	la	$t9,fclose
+$L87:
+	sw	$zero,432($fp)
+$L88:
+	lw	$v0,432($fp)
+	slt	$v0,$v0,100
+	bne	$v0,$zero,$L91
+	b	$L56
+$L91:
+	lw	$v0,432($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$v0,$v0,8
+	lw	$a0,0($v0)
+	la	$t9,free
 	jal	$ra,$t9
-$L45:
+	lw	$v0,432($fp)
+	sll	$v1,$v0,2
+	addu	$v0,$fp,24
+	addu	$v0,$v1,$v0
+	addu	$v0,$v0,416
+	lw	$a0,0($v0)
+	la	$t9,free
+	jal	$ra,$t9
+	lw	$v0,432($fp)
+	addu	$v0,$v0,1
+	sw	$v0,432($fp)
+	b	$L88
+$L56:
 	move	$sp,$fp
-	lw	$ra,664($sp)
-	lw	$fp,660($sp)
-	addu	$sp,$sp,672
+	lw	$ra,1140($sp)
+	lw	$fp,1136($sp)
+	lw	$s0,1128($sp)
+	addu	$sp,$sp,1144
 	j	$ra
 	.end	funcionJoin
 	.size	funcionJoin, .-funcionJoin
@@ -719,18 +942,18 @@ comparaClaves:
 	lw	$a1,52($fp)
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L64
+	bne	$v0,$zero,$L93
 	sw	$zero,24($fp)
-$L64:
+$L93:
 	lw	$v0,56($fp)
-	bne	$v0,$zero,$L65
+	bne	$v0,$zero,$L94
 	lw	$a0,48($fp)
 	lw	$a1,52($fp)
 	la	$t9,strcasecmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L65
+	bne	$v0,$zero,$L94
 	sw	$zero,24($fp)
-$L65:
+$L94:
 	lw	$v0,24($fp)
 	move	$sp,$fp
 	lw	$ra,40($sp)
@@ -739,4 +962,49 @@ $L65:
 	j	$ra
 	.end	comparaClaves
 	.size	comparaClaves, .-comparaClaves
+	.align	2
+	.globl	comparaClavesConsec
+	.ent	comparaClavesConsec
+comparaClavesConsec:
+	.frame	$fp,56,$ra		# vars= 16, regs= 3/0, args= 16, extra= 8
+	.mask	0xd0000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.cpload	$t9
+	.set	reorder
+	subu	$sp,$sp,56
+	.cprestore 16
+	sw	$ra,48($sp)
+	sw	$fp,44($sp)
+	sw	$gp,40($sp)
+	move	$fp,$sp
+	sw	$a0,56($fp)
+	sw	$a1,60($fp)
+	sw	$a2,64($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,24($fp)
+	lw	$a0,56($fp)
+	la	$t9,atoi
+	jal	$ra,$t9
+	sw	$v0,28($fp)
+	lw	$a0,60($fp)
+	la	$t9,atoi
+	jal	$ra,$t9
+	sw	$v0,32($fp)
+	lw	$v0,32($fp)
+	addu	$v0,$v0,1
+	sw	$v0,32($fp)
+	lw	$v1,28($fp)
+	lw	$v0,32($fp)
+	bne	$v1,$v0,$L97
+	sw	$zero,24($fp)
+$L97:
+	lw	$v0,24($fp)
+	move	$sp,$fp
+	lw	$ra,48($sp)
+	lw	$fp,44($sp)
+	addu	$sp,$sp,56
+	j	$ra
+	.end	comparaClavesConsec
+	.size	comparaClavesConsec, .-comparaClavesConsec
 	.ident	"GCC: (GNU) 3.3.3 (NetBSD nb3 20040520)"
