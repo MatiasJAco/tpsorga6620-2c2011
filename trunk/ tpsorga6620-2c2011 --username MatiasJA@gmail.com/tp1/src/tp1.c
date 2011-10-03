9ab22c2e135a627d82d9a5118c5774b28cac2f9f
maldonado.celeste@gmail.com <maldonado.celeste@gmail.com@a299ed4a-ec92-d95f-397f-5777b1f096ef>
2011-10-03 02:21:23 +0000

    subi el esquema del tp...
    en tp1.c pueden implementarlo primero en c, lo que tiene que hacer cada funcion.
    Yo voy a implementarlo ahi primero.
    
    despues cuando compilen con el gcc se puede usar tp1.S o tp1.c con el mismo main.
    
    use en el main el ejemplo que dieron en el enunciado.

---
+++ trunk/ tpsorga6620-2c2011 --username MatiasJA@gmail.com/tp1/src/tp1.c
@@ -0,0 +1,19 @@
+#include "tp1.h"
+
+
+int Push(double *topePila,int tamanioPila,int *cantidadElementos,double valor)
+{
+	return 1;
+}
+
+int Pop(double *topePila,int tamanioPila,int *cantidadElementos,double *valor)
+{
+	return 1;
+}
+
+int SumaTotal(double *topePila,int tamanioPila, int *cantidadElementos, double *resultadoSuma)
+{
+
+
+	return 1;
+}
